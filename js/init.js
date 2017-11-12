398d751060909c172b0492f1aaf48843f34ece4b
Bowlx <krentorr@gmail.com>
2017-11-12 14:52:21 +0300

    meh

---
+++ js/init.js
@@ -0,0 +1,9 @@
+﻿(function ($) {
+    $(function () {
+
+        $('.slider').slider();
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
